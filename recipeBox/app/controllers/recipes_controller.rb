dbaf633454a5c5438f1f5fdef395c05a02255e9e
pdfarhad <pdcsedu@gmail.com>
2015-01-25 00:57:37 +0600

    baal chal

--- recipeBox/app/controllers/recipes_controller.rb
+++ recipeBox/app/controllers/recipes_controller.rb
@@ -1,5 +1,53 @@
 class RecipesController < ApplicationController
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
   def index
-    
+    @recipe = Recipe.all.order("created_at Desc")
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+
+    if @recipe.save
+      redirect_to @recipe, notice: "Succefully created a new recipe"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+  if @recipe.update(recipe_params)
+    redirect_to @recipe
+  else
+    render 'edit'
+  end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path, notice: "Deleted successfully the recipe"
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def show
+
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+
+  end
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+
   end
 end
